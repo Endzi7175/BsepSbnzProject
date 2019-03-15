05c113f0d42dd8d5adba9053908b96b17e534a4e
Natalija Bogdanovic <endzi.7175@gmail.com>
2019-03-15 14:55:43 +0100

    Initial commit

---
+++ src/main/java/com/bsepsbnz/BsepSbnzProject/BsepSbnzProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bsepsbnz.BsepSbnzProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsepSbnzProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsepSbnzProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsepsbnz/BsepSbnzProject/BsepSbnzProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bsepsbnz.BsepSbnzProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BsepSbnzProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
